3f4b945026fa2452afa067899fc72714966aeabd
Jörn Franke <jornfranke@gmail.com>
2016-12-30 12:14:27 +0100

    Refactored to prepare for new mapreduce.* api

--- fileformat/src/main/java/org/zuinnote/hadoop/office/format/mapred/AbstractSpreadSheetDocumentFileOutputFormat.java
+++ fileformat/src/main/java/org/zuinnote/hadoop/office/format/mapred/AbstractSpreadSheetDocumentFileOutputFormat.java
@@ -18,21 +18,16 @@ package org.zuinnote.hadoop.office.format.mapred;
 
 
 import java.io.IOException;
-import java.io.DataOutputStream;
 
-
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.util.Progressable;
+
+import org.apache.hadoop.fs.FileSystem;
 
 import org.apache.hadoop.mapred.RecordWriter;
-import org.apache.hadoop.util.ReflectionUtils;
-import org.apache.hadoop.util.Progressable;
+
 
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
@@ -49,36 +44,5 @@ private static final Log LOG = LogFactory.getLog(AbstractSpreadSheetDocumentFile
 public abstract RecordWriter<NullWritable,SpreadSheetCellDAO> getRecordWriter(FileSystem ignored, JobConf conf, String name, Progressable progress) throws IOException;
 	
 
-/*
-* Creates for the file to be written and outputstream and takes - depending on the configuration - take of compression. Set for compression the following options:
-* mapreduce.output.fileoutputformat.compress true/false
-* mapreduce.output.fileoutputformat.compress.codec java class of compression codec
-*
-* Note that some formats may use already internal compression so that additional compression does not lead to many benefits
-*
-* @param conf Configuration of Job
-* @param file file to be written
-*
-* @return outputstream of the file
-*
-*/
-
-public DataOutputStream getDataOutputStream(JobConf conf, Path file, Progressable progress) throws IOException {
-if (getCompressOutput(conf)==false) { // uncompressed
-	FileSystem fs = file.getFileSystem(conf);
-        FSDataOutputStream fileOut = fs.create(file, progress);
-	return fileOut;
-} else { // compressed (note partially adapted from TextOutputFormat)
-      Class<? extends CompressionCodec> codecClass = getOutputCompressorClass(conf, GzipCodec.class); // Gzip is default if no other has been selected
-      // create the named codec
-      CompressionCodec codec = ReflectionUtils.newInstance(codecClass, conf);
-      // provide proper file extension
-      Path compressedFile = file.suffix(codec.getDefaultExtension());
-      // build the filename including the extension
-      FileSystem fs = compressedFile.getFileSystem(conf);
-      FSDataOutputStream realFileOut = fs.create(compressedFile, progress);
-      return new DataOutputStream(codec.createOutputStream(realFileOut));
-}
-}
 
 }
