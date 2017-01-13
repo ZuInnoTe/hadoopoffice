4f8ed939648fcdc6700fd5cda2cfebf0ba8da7ca
Jörn Franke <jornfranke@gmail.com>
2017-01-13 20:04:39 +0100

    Added script for junit report publishing

---
+++ scripts/publish-junitreport.sh
@@ -0,0 +1,14 @@
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ZuInnoTe/hadoopoffice gh-pages > /dev/null
+
+# Commit and Push the Changes
+cd gh-pages
+git rm -rf ./javadoc
+mkdir -p ./javadoc/fileformat
+cp -Rf $HOME/fileformat/build/reports/tests ./tests/fileformat
+git add -f .
+git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
