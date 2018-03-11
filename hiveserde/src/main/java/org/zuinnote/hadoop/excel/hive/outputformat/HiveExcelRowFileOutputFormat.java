5484ddedf224aed946be063376c1e769f712b98c
Jörn Franke <jornfranke@gmail.com>
2018-03-11 16:45:52 +0100

    added output path to Hadoop Jobconf for HadoopOffice

--- hiveserde/src/main/java/org/zuinnote/hadoop/excel/hive/outputformat/HiveExcelRowFileOutputFormat.java
+++ hiveserde/src/main/java/org/zuinnote/hadoop/excel/hive/outputformat/HiveExcelRowFileOutputFormat.java
@@ -15,23 +15,45 @@
 **/
 package org.zuinnote.hadoop.excel.hive.outputformat;
 
-import org.apache.hadoop.hive.ql.io.HivePassThroughOutputFormat;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.ql.io.HiveOutputFormat;
+import org.apache.hadoop.hive.ql.io.HivePassThroughRecordWriter;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.NullWritable;
-
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.util.Progressable;
 import org.zuinnote.hadoop.office.format.mapred.ExcelRowFileOutputFormat;
 
 /**
  * We use the HivePassThroughOutputFormat to reduce redundant code
  *
  */
-public class HiveExcelRowFileOutputFormat extends HivePassThroughOutputFormat<NullWritable,ExcelRowFileOutputFormat> {
+public class HiveExcelRowFileOutputFormat extends ExcelRowFileOutputFormat implements HiveOutputFormat<NullWritable,ArrayWritable> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3914655814828214968L;
 
 	public HiveExcelRowFileOutputFormat() {
-		super(new ExcelRowFileOutputFormat());
 		
 	}
 
+	@Override
+	public org.apache.hadoop.hive.ql.exec.FileSinkOperator.RecordWriter getHiveRecordWriter(JobConf jc,
+			Path finalOutPath, Class<? extends Writable> valueClass, boolean isCompressed, Properties tableProperties,
+			Progressable progress) throws IOException {
+		FileSystem fs = finalOutPath.getFileSystem(jc);
+		HiveExcelRowFileOutputFormat.setOutputPath(jc, finalOutPath); // for HadoopOffice
+	    RecordWriter<?, ?> recordWriter = this.getRecordWriter(fs, jc, null, progress);
+	    return new HivePassThroughRecordWriter(recordWriter);
+	}
 
 
 
