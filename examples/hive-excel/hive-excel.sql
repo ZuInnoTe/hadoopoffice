2e2a578221911bb8f7ac42a6e41e185ec94b2d01
Jörn Franke <jornfranke@gmail.com>
2018-09-16 23:53:07 +0200

    adapt to newest version

--- examples/hive-excel/hive-excel.sql
+++ examples/hive-excel/hive-excel.sql
@@ -10,7 +10,7 @@ use excel;
 -- based on example file: https://github.com/ZuInnoTe/hadoopoffice/blob/master/fileformat/src/test/resources/testsimple.xlsx?raw=true
 -- all options: https://github.com/ZuInnoTe/hadoopoffice/wiki/Hive-Serde
 
-create external table ExcelTable(decimalsc1 decimal(3,2), booleancolumn boolean, datecolumn date, stringcolumn string, decimalp8sc3 decimal(8,3), bytecolumn tinyint, shortcolumn smallint, intcolumn int, longcolumn bigint) ROW FORMAT SERDE 'org.zuinnote.hadoop.excel.hive.serde.ExcelSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.office.format.mapred.ExcelFileInputFormat' OUTPUTFORMAT 'org.zuinnote.hadoop.excel.hive.outputformat.HiveExcelRowFileOutputFormat' LOCATION '/user/office/files' TBLPROPERTIES("office.hive.decimalFormat"="DE","hadoopoffice.read.header.read"="true", "hadoopoffice.read.locale.bcp47"="DE","hadoopoffice.write.locale.bcp47"="DE");
+create external table ExcelTable(decimalsc1 decimal(3,2), booleancolumn boolean, datecolumn date, stringcolumn string, decimalp8sc3 decimal(8,3), bytecolumn tinyint, shortcolumn smallint, intcolumn int, longcolumn bigint) ROW FORMAT SERDE 'org.zuinnote.hadoop.excel.hive.serde.ExcelSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.office.format.mapred.ExcelFileInputFormat' OUTPUTFORMAT 'org.zuinnote.hadoop.excel.hive.outputformat.HiveExcelRowFileOutputFormat' LOCATION '/user/office/files' TBLPROPERTIES("hadoopoffice.read.simple.decimalFormat"="DE","hadoopoffice.write.simple.decimalFormat"="DE","hadoopoffice.read.header.read"="true", "hadoopoffice.read.locale.bcp47"="DE","hadoopoffice.write.locale.bcp47"="DE");
 
 -- show schema 
 describe ExcelTable;
@@ -25,7 +25,7 @@ select * from ExcelTable LIMIT 10;
 -- simply insert by using select from any other table
 -- note: we instruct Hive to write the table in : /user/office/output
 -- we use as decimalFormat locale "DE" which is Germany, because it fits to the file of the source table "ExcelTable" defined above
-create  table ExcelOut ROW FORMAT SERDE 'org.zuinnote.hadoop.excel.hive.serde.ExcelSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.office.format.mapred.ExcelFileInputFormat' OUTPUTFORMAT 'org.zuinnote.hadoop.excel.hive.outputformat.HiveExcelRowFileOutputFormat' LOCATION '/user/office/output' TBLPROPERTIES("office.hive.write.defaultSheetName"="FirstSheet","office.hive.write.header"="true", "hadoopoffice.write.mimeType"="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "office.hive.decimalFormat"="DE","hadoopoffice.read.locale.bcp47"="DE","hadoopoffice.write.locale.bcp47"="DE")
+create  table ExcelOut ROW FORMAT SERDE 'org.zuinnote.hadoop.excel.hive.serde.ExcelSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.office.format.mapred.ExcelFileInputFormat' OUTPUTFORMAT 'org.zuinnote.hadoop.excel.hive.outputformat.HiveExcelRowFileOutputFormat' LOCATION '/user/office/output' TBLPROPERTIES("office.hive.write.defaultSheetName"="FirstSheet","hadoopoffice.read.header.read"="true","hadoopoffice.write.header.write"="true", "hadoopoffice.write.mimeType"="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "hadoopoffice.read.simple.decimalFormat"="DE","hadoopoffice.write.simple.decimalFormat"="DE","hadoopoffice.read.locale.bcp47"="DE","hadoopoffice.write.locale.bcp47"="DE")
 AS 
 select * from ExcelTable;
 
--- hiveserde/src/main/resources/hive-deploy.sql
+++ hiveserde/src/main/resources/hive-deploy.sql
@@ -1,10 +1,10 @@
 -- optional: if you need support for digital signatures then add the following libraries
 --- bouncycastle: https://search.maven.org/#search%7Cga%7C1%7Corg.bouncycastle
-add jar /tmp/bcprov-ext-jdk15-on-1.58.jar;
-add jar /tmp/bcpkix-jdk15-on-1.58.jar;
+add jar /tmp/bcprov-ext-jdk15-on-1.60.jar;
+add jar /tmp/bcpkix-jdk15-on-1.60.jar;
 --- xmlsec: https://search.maven.org/#search%7Cga%7C1%7Ca%3A%22xmlsec%22
-add jar /tmp/xmlsec-2.1.0.jar;
+add jar /tmp/xmlsec-2.1.2.jar;
 -- you can find the HadoopOffice libraries here: https://search.maven.org/#search%7Cga%7C1%7Chadoopoffice
 -- note this .jar is a fat jar containing all POI dependencies, because one cannot add them to Hive via ADD JAR individually (due to the way how POI uses the classloader)
-add jar /tmp/hadoopoffice-hiveserde-1.1.0.jar;
+add jar /tmp/hadoopoffice-hiveserde-1.2.0.jar;
 
