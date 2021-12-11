25eb83c7a71774d25cc5f5c38871e57ad698b75f
Jörn Franke <jornfranke@gmail.com>
2021-12-11 12:24:06 +0100

    fix #47

--- hiveserde/src/main/resources/hive-deploy.sql
+++ hiveserde/src/main/resources/hive-deploy.sql
@@ -6,4 +6,4 @@ add jar /tmp/bcpkix-jdk15-on-1.69.jar;
 add jar /tmp/xmlsec-2.2.3.jar;
 -- you can find the HadoopOffice libraries here: https://search.maven.org/#search%7Cga%7C1%7Chadoopoffice
 -- note this .jar is a fat jar containing all POI dependencies, because one cannot add them to Hive via ADD JAR individually (due to the way how POI uses the classloader)
-add jar /tmp/hadoopoffice-hiveserde-1.6.0.jar;
+add jar /tmp/hadoopoffice-hiveserde-1.6.1.jar;
